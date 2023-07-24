053d634e86bf1029d511a597c241be42605623e2
Eugen <vasilenkoevgen88@gmail.com>
2023-07-24 12:33:55 +0300

    21. CSS вложенній слиль

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,7 +1,28 @@
+import s from './Dialogs.module.css'
 
-
-export const Dialogs=()=>{
-    return <div>
-        Dialogs
+export const Dialogs = () => {
+    return <div className={s.dialogues}>
+        <div className={s.dialoguesItems}>
+            <div className={s.dialog + ' ' + s.active}>
+                Andrei
+            </div>
+            <div className={s.dilog}>
+                Ivan
+            </div>
+            <div className={s.dilog}>
+                Sergei
+            </div>
+            <div className={s.dilog}>
+                Andrei
+            </div>
+            <div className={s.dilog}>
+                Oleg
+            </div>
+        </div>
+        <div className={s.messages}>
+            <div className={s.message}>Hi</div>
+            <div className={s.message}>How are you itika</div>
+            <div className={s.message}>You</div>
+        </div>
     </div>
 }

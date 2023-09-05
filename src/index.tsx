08bc98c3a2b0e5830168e367793ba3f3bfe035e2
Eugen <vasilenkoevgen88@gmail.com>
2023-09-05 12:26:42 +0300

    Delete extra subscribe to store

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import './index.css';
 import {store} from './redux/redux-store';
 import './index.css';
-import {rerenderEntireTree} from './render';
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import App from "./App";
 
-store.subscribe(rerenderEntireTree)
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
+);

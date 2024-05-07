959f7056248d948299d7ec117ce262e07c984e41
Eugen <vasilenkoevgen88@gmail.com>
2024-05-07 12:54:27 +0300

    Added icon to li

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -1,33 +1,42 @@
 import React from 'react';
 import s from './Navbar.module.scss';
 import {NavLink} from 'react-router-dom';
+import {PersonIcon} from "../../assets/icons/personIcon";
 
 function Navbar() {
     return (
         <div className={s.wrapperNav}>
-        <nav className={s.nav}>
-            <button className={s.item}>
-                <NavLink className={s.nav} activeClassName={s.active} to={'/profile'}>Profile
-                </NavLink>
-            </button>
-            <button className={`${s.item} ${s.active}`}>
-                <NavLink className={s.nav} activeClassName={s.active} to={'/dialogs'}>Messages
-                </NavLink>
-            </button>
-            <button className={s.item}>
-                <NavLink className={s.nav} activeClassName={s.active} to={'/News'}>News
-                </NavLink>
-            </button>
-            <button className={s.item}>
-                <NavLink className={s.nav} activeClassName={s.active} to={'/Music'}>Music
-                </NavLink>
-            </button>
-            <button className={s.item}>
-                <NavLink className={s.nav} activeClassName={s.active} to={'/Users'}>Users
-                </NavLink>
-            </button>
+            <nav>
+                <ul className={s.nav}>
+                    <li className={s.item}>
+                        <span className={s.icon}>
+                            <PersonIcon/>
+                        </span>
+                        <NavLink className={s.nav } activeClassName={s.active} to={'/profile'}>Profile
+                        </NavLink>
+                    </li>
+                <span className={`${s.item} ${s.active}`}>
+                    <NavLink className={s.nav} activeClassName={s.active} to={'/dialogs'}>Messages
+                    </NavLink>
+                </span>
+                <span className={s.item}>
+                    <NavLink className={s.nav} activeClassName={s.active} to={'/News'}>News
+                    </NavLink>
+                </span>
+                <span className={s.item}>
+                    <NavLink className={s.nav} activeClassName={s.active} to={'/Music'}>Music
+                    </NavLink>
+                </span>
+                {/*<li><a aria-current="page" href="#/profile">*/}
 
-        </nav>
+                {/*    <span>PProfile</span></a></li>*/}
+                <span className={s.item}>
+                    <NavLink className={s.nav} activeClassName={s.active} to={'/Users'}>Users
+                    </NavLink>
+                </span>
+                </ul>
+
+            </nav>
         </div>
     );
 }

cedc207444d6532cde350c8eba77588401064b86
Eugen <vasilenkoevgen88@gmail.com>
2024-06-10 23:23:45 +0300

    feat: component button

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,16 +3,18 @@ import s from './Header.module.scss';
 import {NavLink} from "react-router-dom";
 import {MapDispatchToPropsType, MapStateToPropsType} from "./HeaderContainer";
 import logo from '../../assets/img/ms-icon-310x310.png';
+import { Button } from '../../components/ui/button'
+
 
 function Header(props: MapStateToPropsType & MapDispatchToPropsType) {
     return (
         <header className={s.header}>
             <img className={s.logo}
                 src={logo} alt={'logo'}/>
-            <div className={s.loginBlock}>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} className={s.loginBlock}>
                 {props.isAuth ?
-                    <div><NavLink to={'/login'}>{props.email}</NavLink>
-                        <button onClick={props.logout}>Log out</button>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}><NavLink to={'/login'}>{props.email}</NavLink>
+                        <Button onClick={props.logout}>Log out</Button>
                     </div> :
                     <div>
                     <NavLink to={'/login'}>{"Login"}</NavLink>

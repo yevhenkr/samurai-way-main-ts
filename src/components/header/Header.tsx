482860f1d5f56e890aed52ebf083e475442ac2b7
Eugen <vasilenkoevgen88@gmail.com>
2024-06-10 12:20:21 +0300

    fix: logo icon in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import s from './Header.module.scss';
 import {NavLink} from "react-router-dom";
 import {MapDispatchToPropsType, MapStateToPropsType} from "./HeaderContainer";
+import logo from '../../assets/img/ms-icon-310x310.png';
 
 function Header(props: MapStateToPropsType & MapDispatchToPropsType) {
     return (
         <header className={s.header}>
-            <img
-                src={'https://1000logos.net/wp-content/uploads/2016/10/Batman-Logo-1966.png'} alt={'logo'}/>
+            <img className={s.logo}
+                src={logo} alt={'logo'}/>
             <div className={s.loginBlock}>
                 {props.isAuth ?
                     <div><NavLink to={'/login'}>{props.email}</NavLink>

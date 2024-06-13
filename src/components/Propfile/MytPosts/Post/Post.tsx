083bb33c7fbab789f70c25b46a87ba2d6df42abc
Eugen <vasilenkoevgen88@gmail.com>
2024-06-13 13:28:30 +0300

    fix: style text

--- src/components/Propfile/MytPosts/Post/Post.tsx
+++ src/components/Propfile/MytPosts/Post/Post.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import s from './Post.module.scss';
 
 function Post(props: any) {
-    return (<div  className={s.item}>{props.message}</div>);
+    return (<div>{props.message}</div>);
 }
 
 export default Post;
